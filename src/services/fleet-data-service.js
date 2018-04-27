4e2ab4a16a069a03fbe39ddb5e4e19302d5f8efc
wowdyaln <rogersmk@gmail.com.tw>
2018-04-27 17:18:11 +0800

    27 - Creating Core Classes

--- src/services/fleet-data-service.js
+++ src/services/fleet-data-service.js
@@ -7,5 +7,15 @@ export class FleetDataService {
     this.cars = []
     this.drones = []
   }
-
+  loadData(datas){
+    datas.forEach((data)=>{
+      if (data.type==="drone"){
+        this.drones.push(data)
+      } else if (data.type === "car"){
+        this.cars.push(data)
+      }
+    })
+  }
 }
+
+
