cf65906530d11fc1e124babfb1129999f538db13
wowdyaln <rogersmk@gmail.com.tw>
2018-04-30 14:21:00 +0800

    41 - A Data Table Class. done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import {FleetDataService} from './services/fleet-data-service.js'
 import {Button} from './ui/button.js'
 import {Image} from './ui/image.js'
 import {TitleBar} from './ui/title-bar.js'
+import {DataTable} from './ui/data-table.js'
 
 
 // let dataService = new FleetDataService()
@@ -22,3 +23,10 @@ tb.addLink('Cars','')
 tb.addLink('Drones','')
 tb.addLink('Map','')
 tb.appendToElement($('body'))
+
+let headers = "License Make Model Miles".split(" ")
+let dataService = new FleetDataService()
+dataService.loadData(fleet)
+let dt = new DataTable(headers, dataService.cars)
+
+dt.appendToElement($('body'))
