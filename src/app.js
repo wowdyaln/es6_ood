caa4e652c2f3fb7e53b821aa9f2f6352f0c17a8b
wowdyaln <rogersmk@gmail.com.tw>
2018-04-30 13:02:06 +0800

    39 - An Image Class. done

--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@ import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js'
 import {FleetDataService} from './services/fleet-data-service.js'
 import {Button} from './ui/button.js'
+import {Image} from './ui/image.js'
 
 
 // let dataService = new FleetDataService()
 // dataService.loadData(fleet)
 let b = new Button('click me')
 b.appendToElement($('body'))
+
+// let i = new Image('./image/drone.jpg')
+let i = new Image('../image/drone.jpg')
+i.appendToElement($('body'))
+
+console.log(i)
+console.log(b)
