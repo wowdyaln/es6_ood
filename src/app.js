cba5c5207f1be8c328a2177f1c09eeb7d6f63163
wowdyaln <rogersmk@gmail.com.tw>
2018-04-30 15:42:15 +0800

    46 - An Application Class. done

--- src/app.js
+++ src/app.js
@@ -1,37 +1,16 @@
 import $ from 'jquery';
-import {Car} from './classes/car.js';
-import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js'
 import {FleetDataService} from './services/fleet-data-service.js'
-import {Button} from './ui/button.js'
-import {Image} from './ui/image.js'
-import {TitleBar} from './ui/title-bar.js'
-import {DataTable} from './ui/data-table.js'
-import { GoogleMap } from './ui/google-map.js';
+import {ApplicationBase} from './framework/application-base.js'
 
+export class App extends ApplicationBase {
 
-// let dataService = new FleetDataService()
-// dataService.loadData(fleet)
-// let b = new Button('click me')
-// b.appendToElement($('body'))
+  constructor(){
+    super('fleet manager')
+    this.dataService = new FleetDataService()
+    this.dataService.loadData(fleet)
+  }
+}
 
-// let i = new Image('../image/drone.jpg')
-// i.appendToElement($('body'))
-
-// let tb = new TitleBar('our application')
-// tb.addLink('Home','')
-// tb.addLink('Cars','')
-// tb.addLink('Drones','')
-// tb.addLink('Map','')
-// tb.appendToElement($('body'))
-
-// let headers = "License Make Model Miles".split(" ")
-let dataService = new FleetDataService()
-dataService.loadData(fleet)
-// let dt = new DataTable(headers, dataService.cars)
-// dt.appendToElement($('body'))
-
-let centerOfMap = {lat: 40.78, lng: -73.96}
-
-let map = new GoogleMap(centerOfMap, dataService.cars)
-map.appendToElement($('body'))
+export let application = new App()
+application.show($('body'))
